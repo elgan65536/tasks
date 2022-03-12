18755352e4dab19e3276655230154fafa61fc995
elgan65536 <elgan256@gmail.com>
2022-03-12 10:45:16 -0500

    giveattempts passes test

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -5,22 +5,60 @@ type ChangeEvent = React.ChangeEvent<
     HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
 >;
 
+interface buttonProps {
+    setAttempts: (newAttempts: number) => void;
+    attempts: number;
+    amount: number;
+}
+
+function Gainer(props: buttonProps): JSX.Element {
+    return (
+        <Button
+            onClick={() => props.setAttempts(props.attempts + props.amount)}
+        >
+            gain
+        </Button>
+    );
+}
+
+function User(props: buttonProps): JSX.Element {
+    return (
+        <Button
+            onClick={() => props.setAttempts(props.attempts - 1)}
+            disabled={props.attempts <= 0}
+        >
+            use
+        </Button>
+    );
+}
+
 export function GiveAttempts(): JSX.Element {
     const [attempts, setAttempts] = useState<number>(3);
-    function updateAttempts(event: ChangeEvent) {
-        setAttempts(parseInt(event.target.value));
+    const [gainAmount, setGainAmount] = useState<number>(0);
+    function updateAmount(event: ChangeEvent) {
+        setGainAmount(parseInt(event.target.value) || 0);
     }
     return (
         <div>
             <h3>Give Attempts</h3>
             <Form.Group controlId="formAttempts">
-                <Form.Label>Attempts:</Form.Label>
-                <Form.Control value={attempts} onChange={updateAttempts} />
+                <Form.Label>Attempts: {attempts}</Form.Label>
+                <Form.Control
+                    value={gainAmount}
+                    onChange={updateAmount}
+                    type="number"
+                />
             </Form.Group>
-            <Button onClick={() => setAttempts(attempts - 1)}>use</Button>
-            <Button onClick={() => setAttempts(attempts + 1)}>gain</Button>
-            <br />
-            {attempts}
+            <Gainer
+                attempts={attempts}
+                setAttempts={setAttempts}
+                amount={gainAmount}
+            ></Gainer>
+            <User
+                attempts={attempts}
+                setAttempts={setAttempts}
+                amount={-1}
+            ></User>
         </div>
     );
 }
